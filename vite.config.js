764e9264c17ffba3903650b3c8b1921dbeb94cb6
naruapon <naruapon@webmail.npru.ac.th>
2025-03-11 19:10:07 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/vue-github-app/',
+  base: '/vue-app/',
   plugins: [vue()],
   resolve: {
     alias: {
